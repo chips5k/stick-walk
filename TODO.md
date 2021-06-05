599295a97497cdfb409054240332b055d9dbd5ab
Callem Pittard <callem.pittard@gmail.com>
2021-06-05 23:20:56 +1000

    rough game loop

---
+++ TODO.md
@@ -0,0 +1,4 @@
+setup basic game/physics/render loop
+
+implement skeletons, use edge detection/adjustment approach prob need to lift a bunch of stuff from other repos
+implement ga controls for tendons/muscles
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, RefObject, memo } from "react";
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
@@ -10,27 +10,78 @@ const StyledCanvas = styled.canvas`
   border: 2px solid #ccc;
 `;
 
-const AppCanvas = () => {
-  const ref = useRef<HTMLCanvasElement>(null);
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const UPDATES_PER_SECOND = 60;
 
+const AppCanvas = memo(({ delayRef }: { delayRef: RefObject<number> }) => {
+  const ref = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-    const canvas = ref.current;
-    const ctx = canvas?.getContext("2d");
-    if (ctx) {
-      ctx.fillStyle = "#ddd";
-      ctx.fillRect(20, 20, 300, 300);
-    }
+    (async () => {
+      const canvas = ref.current;
+      const ctx = canvas?.getContext("2d");
+      const running = true;
+
+      if (ctx) {
+        const next = async (
+          accumulatedMs: number = 0,
+          previousFrameStartMs: number = performance.now()
+        ) => {
+          const currentFrameStartMs = performance.now();
+          // Delta between previous and current frames
+          const elapsedSincePreviousFrameMs =
+            currentFrameStartMs - previousFrameStartMs;
+          accumulatedMs += elapsedSincePreviousFrameMs;
+          const physicsTimeMs = 1000 / UPDATES_PER_SECOND;
+          let updates = 0;
+          while (accumulatedMs / physicsTimeMs >= 1) {
+            accumulatedMs -= physicsTimeMs;
+            updates++;
+          }
+
+          await sleep(delayRef.current ?? 0);
+          // Render
+          ctx.clearRect(0, 0, 800, 600);
+          ctx.fillStyle = "#ddd";
+          ctx.fillRect(20, 20, 300, 300);
+
+          const fps = Math.round(1000 / elapsedSincePreviousFrameMs);
+          ctx.fillStyle = "#459617";
+          ctx.fillText(`FPS: ${fps}`, 10, 10);
+          ctx.fillText(`UPS: ${updates}`, 60, 10);
+
+          if (running) {
+            window.requestAnimationFrame(() => {
+              next(accumulatedMs, currentFrameStartMs);
+            });
+          }
+        };
+
+        next();
+      }
+    })();
   }, [ref]);
 
   return <StyledCanvas width={800} height={600} ref={ref} />;
-};
+});
 
 function App() {
+  const delayRef = useRef(0);
   return (
     <StyledContainer>
-      <h1>Stick Walk</h1>
-      <p>Teach a stickman to walk using evolutionary programming programming</p>
-      <AppCanvas />
+      <div>
+        <h1>Stick Walk</h1>
+        <p>
+          Teach a stickman to walk using evolutionary programming programming
+        </p>
+        Frame delay:
+        <input
+          type="text"
+          onChange={(e) => {
+            delayRef.current = parseInt(e.target.value) | 0;
+          }}
+        />
+      </div>
+      <AppCanvas delayRef={delayRef} />
     </StyledContainer>
   );
 }
